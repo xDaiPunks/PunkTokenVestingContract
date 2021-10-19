4397af520ddf943e619b6bfa4eed4ddac8a714b7
olibaron <olivier@airwave,io>
2021-10-19 15:34:21 +0200

    initial migrations update

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,5 +1,21 @@
-const Punk = artifacts.require('Punk');
+const Vesting = artifacts.require('Vesting');
+
+const start = 1634141102;
+const cliff = 0;
+const duration = 600;
+const totalAvailable = 100000000000000000000000000;
+const PunkContractAddress = '0xDB2a4Df88a3a5a3Ca1aDe9F3303Bb4fC98DefE35';
+const PunksContractAddress = '0xEFb049408E3764951bFC7a64EeD02F532569d69A';
 
 module.exports = function (deployer) {
-  deployer.deploy(Punk, { gas: 5000000 });
+  deployer.deploy(
+    Vesting,
+    start,
+    cliff,
+    duration,
+    totalAvailable,
+    PunkContractAddress,
+    PunksContractAddress,
+    { gas: 5000000 }
+  );
 };
